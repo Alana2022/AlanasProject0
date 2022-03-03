99a358542f233fa6efbcecd5cf3227fd852c531b
lana2022 <lindenstudio17@gmail.com>
2022-03-03 00:01:28 -0500

    Project 0 wire frame

---
+++ main.js
@@ -0,0 +1,5 @@
+//console.log("hello")
+
+const wakeEl = document.getElementById("btn-wake");
+const sleepEl = document.getElementById("btn-sleep");
+const startEl = document.getElementById("btn-start");
